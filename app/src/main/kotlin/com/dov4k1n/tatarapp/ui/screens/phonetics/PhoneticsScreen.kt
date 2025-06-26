2d5a56dc7f3f1e720a451747ca0dcd13dfd3fbcd
Ayzat Rizatdinov <ddov4k1n@gmail.com>
2025-06-27 00:10:19 +0300

    add copyright notice everywhere

--- app/src/main/kotlin/com/dov4k1n/tatarapp/ui/screens/phonetics/PhoneticsScreen.kt
+++ app/src/main/kotlin/com/dov4k1n/tatarapp/ui/screens/phonetics/PhoneticsScreen.kt
@@ -1,3 +1,22 @@
+/*
+ * Tatarcham v0.2, tatar language learning app for Android.
+ * Copyright (C) 2023-2025 Ayzat Rizatdinov <ddov4k1n at gmail dot com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ *
+ */
+
 package com.dov4k1n.tatarapp.ui.screens.phonetics
 
 import androidx.compose.foundation.layout.Column
